50f86d56032a1ae3e7db8c65528d3d4277455b62
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2021-12-21 19:30:28 +0100

    Added a json server that will hold user data
    Added the modals for the search and profile icons
    
    Added an arrow that allows user to return to the previous page

--- index.js
+++ index.js
@@ -5,7 +5,37 @@ const state = {
   selectedArea: null,
   selectedCategory: null,
   foodIdsOfSelectedArea: [],
-  foodListByArea: []
+  foodListByArea: [],
+  search: '',
+  selectedModal: '',
+  users: [],
+  user: null
+}
+// fetch users from the server
+function getUsers() {
+  return fetch('http://localhost:3000/users').then(resp => resp.json())
+  .then(users => state.users = users)
+}
+// checks if the email and password match the user's input in sign-in form
+function signInUser(email, password) {
+  for(const user of state.users){
+      if(user.id === email && user.password === password) {
+          state.user = user
+      }
+  }
+}
+// adds a new user using sign-up form
+function addUser(name, lastName, email, password) {
+
+  state.users.push({firstName: name, lastName: lastName, id: email, password: password})
+
+  return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({firstName: name, lastName: lastName, id: email, password: password, bag: []})
+  }).then(resp => resp.json())
 }
 // gets the ids of every food in selected area and pushes to state
 function getIdsOfSelectedArea() {
@@ -32,9 +62,10 @@ function getFoodsOfSelectedArea() {
 // displays the food list items on the main page
 function getFoodsToDisplay() {
   let foodsToDisplay = state.foodListByArea
-  foodsToDisplay = foodsToDisplay.filter(food => 
-    state.selectedCategory === food.strCategory
-  )
+  foodsToDisplay = foodsToDisplay.filter(food =>{
+    console.log(food[0].strCategory, state.selectedCategory === food[0].strCategory);
+    state.selectedCategory === food[0].strCategory
+  })
   return foodsToDisplay
 }
 // get the full list of categories the user can choose
@@ -75,13 +106,13 @@ function listenToFilterByCategory(filterByCategorySelect) {
 // listens to 'Click me for food' button
 function listenToFoodButton() {
   document.body.innerHTML = ''
-  state.selectedPage = 'MainPage'
+  state.selectedPage = 'mainPage'
   render()
 }
 // listens to click of a food list element
 function listenToProductLi() {
   document.body.innerHTML = ''
-  state.selectedPage = 'FoodDetailsPage'
+  state.selectedPage = 'foodDetailsPage'
   render()
 }
 // renders the category form list
@@ -194,6 +225,24 @@ function renderWelcomePage() {
 function renderMainPage() {
   const mainPage = document.createElement('section')
   mainPage.setAttribute('class', 'main-page')
+
+  const previousPageButton = document.createElement("button");
+  previousPageButton.setAttribute("class", "previous-button");
+
+  previousPageButton.addEventListener("click", function () {
+    document.body.innerHTML = "";
+    state.selectedPage = "welcomePage";
+    render();
+  });
+
+  const backArrowImg = document.createElement("img");
+  backArrowImg.setAttribute("class", "back-arrow");
+  backArrowImg.setAttribute("src", "./back-arrow1.png");
+
+  backArrowImg.setAttribute("alt", "back-arrow");
+
+  previousPageButton.append(backArrowImg);
+
   const mainTitleMainPage = document.createElement("h1");
   mainTitleMainPage.setAttribute("class", "main-title");
   mainTitleMainPage.textContent = `Mmm... Don't they look delicious!!`;
@@ -221,13 +270,31 @@ function renderMainPage() {
     foodListUl.append(foodListLi);
   }
   foodListSection.append(foodListUl);
-  mainPage.append(mainTitleMainPage, foodListSection)
+  mainPage.append(previousPageButton, mainTitleMainPage, foodListSection)
   document.body.append(renderTheHeader(),mainPage)
 }
 // ****************FOOD DETAILS PAGE*****************
 function renderFoodDetails() {
   const foodDetailsPage = document.createElement('section')
   foodDetailsPage.setAttribute('class', 'food-details-page')
+  
+  const previousPageButton = document.createElement("button");
+  previousPageButton.setAttribute("class", "previous-button");
+
+  previousPageButton.addEventListener("click", function () {
+    document.body.innerHTML = "";
+    state.selectedPage = "MainPage";
+    render();
+  });
+
+  const backArrowImg = document.createElement("img");
+  backArrowImg.setAttribute("class", "back-arrow");
+  backArrowImg.setAttribute("src", "./back-arrow1.png");
+
+  backArrowImg.setAttribute("alt", "back-arrow");
+
+  previousPageButton.append(backArrowImg);
+  
   const mainTitleFoodPage = document.createElement("h1");
   mainTitleFoodPage.setAttribute("class", "main-title");
   mainTitleFoodPage.textContent = `Lets Cook!!`;
@@ -244,7 +311,7 @@ function renderFoodDetails() {
   foodPageLi.textContent = "Ingredients";
 
   foodPageUl.append(foodPageLi);
-  foodImgAndIngredients.append(imageOfFood, foodPageUl);
+  foodImgAndIngredients.append(previousPageButton, imageOfFood, foodPageUl);
 
   const writtenAndVideoInstructon = document.createElement("div");
   writtenAndVideoInstructon.setAttribute("class", "written-and-video-instructions");
@@ -276,34 +343,286 @@ function renderTheHeader() {
   magnifyingGlass.setAttribute("class", "magnifying-glass");
   magnifyingGlass.setAttribute("src", "https://img.icons8.com/material-outlined/50/000000/search--v1.png");
   magnifyingGlass.setAttribute("alt", "maginfyi");
-
+  magnifyingGlass.addEventListener('click', function() {
+    state.selectedModal = 'search'
+    render()
+  })
   const signIn = document.createElement("img");
   signIn.setAttribute("class", "signin");
   signIn.setAttribute("src", "https://img.icons8.com/small/50/000000/gender-neutral-user.png");
   signIn.setAttribute("alt", "maginfyi");
+  signIn.addEventListener('click', function() {
+    state.selectedModal = 'profile'
+    render()
+  })
 
   const bagImage = document.createElement("img");
   bagImage.setAttribute("class", "bag-image");
   bagImage.setAttribute("src", "https://img.icons8.com/material-outlined/24/000000/shopping-bag--v1.png");
   bagImage.setAttribute("alt", "maginfyi");
+  bagImage.addEventListener('click', function() {
+    state.selectedModal = 'bag'
+    render()
+  })
 
   rightElementsOfHeader.append(magnifyingGlass, signIn, bagImage);
 
   headerOfPage.append(rightElementsOfHeader);
   return headerOfPage;
 }
+
+function renderSearchModal() {
+  const modalWrapper = document.createElement('div')
+  modalWrapper.setAttribute('class', 'modal-wrapper')
+  modalWrapper.addEventListener('click', function () {
+      state.selectedModal = ''
+      render()
+  })
+
+  const modalEl = document.createElement('div')
+  modalEl.setAttribute('class', 'modal')
+  modalEl.addEventListener('click', function (event) {
+      event.stopPropagation()
+  })
+
+  const closeModalBtn = document.createElement('button')
+  closeModalBtn.setAttribute('class', 'modal__close-btn')
+  closeModalBtn.textContent = 'X'
+  closeModalBtn.addEventListener('click', function () {
+      state.selectedModal = ''
+      render()
+  })
+
+  const searchModalTitle = document.createElement('h2')
+  searchModalTitle.textContent = 'Search for your favorite items!'
+  const searchModalForm = document.createElement('form')
+  searchModalForm.addEventListener('submit', function (event) {
+      event.preventDefault()
+      state.search = searchModalInput.value
+
+      state.selectedModal = ''
+      render()
+  })
+
+  const searchModalInput = document.createElement('input')
+  searchModalInput.setAttribute('class', 'modal__input')
+  searchModalInput.setAttribute('placeholder', 'Search...')
+
+  searchModalForm.append(searchModalInput)
+  modalEl.append(closeModalBtn, searchModalTitle, searchModalForm)
+  modalWrapper.append(modalEl)
+
+  document.body.append(modalWrapper)
+}
+
+function renderProfileModalForSignUp(modalEl, modalWrapper) {
+  modalEl.innerHTML = ''
+  const closeModalBtn = document.createElement('button')
+  closeModalBtn.setAttribute('class', 'modal__close-btn')
+  closeModalBtn.textContent = 'X'
+  closeModalBtn.addEventListener('click', function () {
+      state.selectedModal = ''
+      render()
+  })
+
+  const profileModalTitle = document.createElement('h2')
+  profileModalTitle.textContent = 'Sign Up'
+  const profileModalForm = document.createElement('form')
+  profileModalForm.addEventListener('submit', function (event) {
+      event.preventDefault()
+      state.search = searchModalInput.value
+
+      state.selectedModal = ''
+      render()
+  })
+
+  const profileModalNameInput = document.createElement('input')
+  profileModalNameInput.setAttribute('class', 'modal__input sign-in')
+  profileModalNameInput.setAttribute('name', 'name')
+  profileModalNameInput.setAttribute('type', 'text')
+  const profileModalNameLabel = document.createElement('label')
+  profileModalNameLabel.setAttribute('for', 'name')
+  profileModalNameLabel.textContent = 'Enter your name'
+
+  const profileModalLastNameInput = document.createElement('input')
+  profileModalLastNameInput.setAttribute('class', 'modal__input sign-in')
+  profileModalLastNameInput.setAttribute('name', 'last-name')
+  profileModalLastNameInput.setAttribute('type', 'text')
+  const profileModalLastNameLabel = document.createElement('label')
+  profileModalLastNameLabel.setAttribute('for', 'last-name')
+  profileModalLastNameLabel.textContent = 'Enter your last name'
+
+  const profileModalEmailInput = document.createElement('input')
+  profileModalEmailInput.setAttribute('type', 'email')
+  profileModalEmailInput.setAttribute('class', 'modal__input sign-in')
+  profileModalEmailInput.setAttribute('name', 'email')
+  const profileModalEmailLabel = document.createElement('label')
+  profileModalEmailLabel.setAttribute('for', 'email')
+  profileModalEmailLabel.textContent = 'Enter Email'
+
+  const profileModalPswInput = document.createElement('input')
+  profileModalPswInput.setAttribute('type', 'password')
+  profileModalPswInput.setAttribute('class', 'modal__input sign-in')
+  profileModalPswInput.setAttribute('name', 'psw')
+  const profileModalPswLabel = document.createElement('label')
+  profileModalPswLabel.setAttribute('for', 'psw')
+  profileModalPswLabel.textContent = 'Create Password'
+
+  const profileModalSubmitBtn = document.createElement('button')
+  profileModalSubmitBtn.setAttribute('class', 'modal__submit-button')
+  profileModalSubmitBtn.setAttribute('type', 'submit')
+  profileModalSubmitBtn.textContent = 'SIGN UP'
+  profileModalSubmitBtn.addEventListener('click', event => {
+      event.preventDefault()
+      addUser(profileModalNameInput.value, profileModalLastNameInput.value, profileModalEmailInput.value, profileModalPswInput.value)
+  })
+
+  profileModalForm.append(profileModalNameLabel, profileModalNameInput, profileModalLastNameLabel, profileModalLastNameInput, profileModalEmailLabel, profileModalEmailInput, profileModalPswLabel, profileModalPswInput, profileModalSubmitBtn)
+  modalEl.append(closeModalBtn, profileModalTitle, profileModalForm)
+  modalWrapper.append(modalEl)
+
+  document.body.append(modalWrapper)
+}
+
+function renderProfileModal() {
+  const modalWrapper = document.createElement('div')
+  modalWrapper.setAttribute('class', 'modal-wrapper')
+  modalWrapper.addEventListener('click', function () {
+      state.selectedModal = ''
+      render()
+  })
+
+  const modalEl = document.createElement('div')
+  modalEl.setAttribute('class', 'modal')
+  modalEl.addEventListener('click', function (event) {
+      event.stopPropagation()
+  })
+
+  const closeModalBtn = document.createElement('button')
+  closeModalBtn.setAttribute('class', 'modal__close-btn')
+  closeModalBtn.textContent = 'X'
+  closeModalBtn.addEventListener('click', function () {
+      state.selectedModal = ''
+      render()
+  })
+
+  
+      const profileModalTitle = document.createElement('h2')
+      profileModalTitle.textContent = 'Sign In'
+      const profileModalForm = document.createElement('form')
+      profileModalForm.addEventListener('submit', function (event) {
+          event.preventDefault()
+          //state.search = searchModalInput.value
+
+          state.selectedModal = ''
+          render()
+      })
+
+      const profileModalEmailInput = document.createElement('input')
+      profileModalEmailInput.setAttribute('type', 'email')
+      profileModalEmailInput.setAttribute('class', 'modal__input sign-in')
+      profileModalEmailInput.setAttribute('name', 'email')
+      const profileModalEmailLabel = document.createElement('label')
+      profileModalEmailLabel.setAttribute('for', 'email')
+      profileModalEmailLabel.textContent = 'Email'
+
+      const profileModalPswInput = document.createElement('input')
+      profileModalPswInput.setAttribute('type', 'password')
+      profileModalPswInput.setAttribute('class', 'modal__input sign-in')
+      profileModalPswInput.setAttribute('name', 'psw')
+      const profileModalPswLabel = document.createElement('label')
+      profileModalPswLabel.setAttribute('for', 'psw')
+      profileModalPswLabel.textContent = 'Password'
+
+      const profileModalSubmitBtn = document.createElement('button')
+      profileModalSubmitBtn.setAttribute('class', 'modal__submit-button')
+      profileModalSubmitBtn.setAttribute('type', 'submit')
+      profileModalSubmitBtn.textContent = 'SIGN IN'
+      profileModalSubmitBtn.addEventListener('click', function(){
+          signInUser(profileModalEmailInput.value, profileModalPswInput.value)
+          state.selectedModal = ''
+          state.bag = state.user.bag
+          render()
+      })
+
+      const signUpRedirect = document.createElement('p')
+      signUpRedirect.setAttribute('class', 'modal__sign-up')
+      signUpRedirect.textContent = "Don't have an account yet? Create one here!"
+      signUpRedirect.addEventListener('click', function () {
+          renderProfileModalForSignUp(modalEl, modalWrapper)
+      })
+
+      profileModalForm.append(profileModalEmailLabel, profileModalEmailInput, profileModalPswLabel, profileModalPswInput, profileModalSubmitBtn, signUpRedirect)
+      modalEl.append(closeModalBtn, profileModalTitle, profileModalForm)
+  
+  modalWrapper.append(modalEl)
+
+  document.body.append(modalWrapper)
+}
+
+function renderProfileModalWhenSignedIn() {
+  const modalWrapper = document.createElement('div')
+  modalWrapper.setAttribute('class', 'modal-wrapper')
+  modalWrapper.addEventListener('click', function () {
+      state.selectedModal = ''
+      render()
+  })
+
+  const modalEl = document.createElement('div')
+  modalEl.setAttribute('class', 'modal')
+  modalEl.addEventListener('click', function (event) {
+      event.stopPropagation()
+  })
+
+  const closeModalBtn = document.createElement('button')
+  closeModalBtn.setAttribute('class', 'modal__close-btn')
+  closeModalBtn.textContent = 'X'
+  closeModalBtn.addEventListener('click', function () {
+      state.selectedModal = ''
+      render()
+  })
+
+  const profileModalTitle = document.createElement('h2')
+  profileModalTitle.textContent = `${state.user.firstName} ${state.user.lastName}`
+  const profileModalLogOut = document.createElement('p')
+  profileModalLogOut.setAttribute('class', 'modal__sign-up')
+  profileModalLogOut.textContent = "Log Out"
+  profileModalLogOut.addEventListener('click', function() {
+      state.user = null
+      state.bag = []
+      state.selectedModal = ''
+      render()
+  })
+
+  
+  modalEl.append(closeModalBtn, profileModalTitle, profileModalLogOut)
+  modalWrapper.append(modalEl)
+
+  document.body.append(modalWrapper)
+}
+
 // selects which page to display
 function selectPageToDisplay() {
   if(state.selectedPage === 'welcomePage') renderWelcomePage()
-  else if(state.selectedPage === 'MainPage') renderMainPage()
-  else if(state.selectedPage === 'FoodDetailsPage') renderFoodDetails()
+  else if(state.selectedPage === 'mainPage') renderMainPage()
+  else if(state.selectedPage === 'foodDetailsPage') renderFoodDetails()
+}
+// picks which one of the modals to show
+function renderModal() {
+  if (state.selectedModal === '') return
+  if (state.selectedModal === 'search') renderSearchModal()
+  if (state.user !== null && state.selectedModal === 'profile') renderProfileModalWhenSignedIn()
+  if (state.user == null && state.selectedModal === 'profile') renderProfileModal()
+  
 }
 function render() {
   selectPageToDisplay()
+  renderModal()
 }
 function init() {
   getCategories()
   getAreas()
+  getUsers()
 }
 
 init()
